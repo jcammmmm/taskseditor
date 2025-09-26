8477511399979bed5b1b1c88cbe0a7dd745fb2ed
Juan Camilo Florez <jcflorezv@unal.edu.co>
2025-09-26 08:15:20 -0500

    load button loads content into the editor

--- monacoeditor/example/index.js
+++ monacoeditor/example/index.js
@@ -29,11 +29,48 @@ function save() {
   });
 }
 
+/**
+ * This function is executed when the user clicks on the 'Load' button.
+ * It Downloads the main.tks file from the server, reads its contents
+ * and then sets this value to the monaco editor.
+ */
 function load() {
-  fetch('http://localhost/taskseditor/tasks/main.tks', {
-    method: 'GET'
-  })
+  // Taken from https://developer.mozilla.org/en-US/docs/Web/API/ReadableStream#examples
+  fetch('http://localhost/taskseditor/tasks/main.tks')
+    .then((response) => response.body)
+    .then((rb) => {
+      const reader = rb.getReader();
 
+      return new ReadableStream({
+        start(controller) {
+          // The following function handles each data chunk
+          function push() {
+            // "done" is a Boolean and value a "Uint8Array"
+            reader.read().then(({ done, value }) => {
+              // If there is no more data to read
+              if (done) {
+                console.log("done", done);
+                controller.close();
+                return;
+              }
+              // Get the data and send it to the browser via the controller
+              controller.enqueue(value);
+              // Check chunks by logging to the console
+              console.log(done, value);
+              push();
+            });
+          }
+          push();
+        },
+      });
+    })
+    .then((stream) =>
+    // Respond with our stream
+    new Response(stream, { headers: { "Content-Type": "text/html" } }).text(),
+  )
+  .then((result) => {
+    editor.setValue(result);
+  });
 
   var pre = document.getElementsByTagName("pre");
   pre[0].innerHTML = "OGROSITO!!!"
