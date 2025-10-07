4f6ec218f3f3f457897bc1b0a278759403f5f2d4
Juan Camilo Florez <jcflorezv@unal.edu.co>
2025-10-07 14:45:03 -0500

    add more load buttons and save status label

--- editor/comm.js
+++ editor/comm.js
@@ -44,32 +44,6 @@ function load() {
   });
 }
 
-editor.setTheme("ace/theme/dracula");
-editor.session.setMode("ace/mode/text");
-
-var editorplaceholder = document.getElementById("editor");
-
-var loadButton = document.createElement("button");
-loadButton.innerHTML = "Load";
-loadButton.onclick = load;
-document.body.insertBefore(loadButton, editorplaceholder)
-
-var undoButton = document.createElement("button");
-undoButton.innerHTML = "Undo";
-undoButton.onclick = function() { editor.undo() };
-document.body.insertBefore(undoButton, editorplaceholder)
-
-var redoButton = document.createElement("button");
-redoButton.innerHTML = "Redo";
-redoButton.onclick = function() { editor.redo() };
-document.body.insertBefore(redoButton, editorplaceholder)
-
-var refs = {
-  loadButton: loadButton,
-  undoButton: undoButton,
-  redoButton: redoButton,
-}
-
 function updateToolbar() {
   refs.undoButton.disabled = !editor.session.getUndoManager().hasUndo();
   refs.redoButton.disabled = !editor.session.getUndoManager().hasRedo();
@@ -84,11 +58,64 @@ function save() {
   fetch(`${PROTO}://${HOST}/script/save.py`, {
     method: 'POST',
     body: content
+  }).then(() => {
+    saveStatus.innerHTML = "saved.";
+    saveStatus.style = "color: green"
   });
 }
 
+function setUnsave() {
+  saveStatus.innerHTML = "not saved.";
+  saveStatus.style = "color: red"
+}
+
+editor.setTheme("ace/theme/dracula");
+editor.session.setMode("ace/mode/text");
+
+var editorplaceholder = document.getElementById("editor");
+
+var loadButton = document.createElement("button");
+loadButton.innerHTML = "Tasks";
+loadButton.onclick = load;
+document.body.insertBefore(loadButton, editorplaceholder)
+
+var loadButton = document.createElement("button");
+loadButton.innerHTML = "Plans";
+loadButton.onclick = load;
+document.body.insertBefore(loadButton, editorplaceholder)
+
+var loadButton = document.createElement("button");
+loadButton.innerHTML = "Food";
+loadButton.onclick = load;
+document.body.insertBefore(loadButton, editorplaceholder)
+
+var undoButton = document.createElement("button");
+undoButton.innerHTML = "Undo";
+undoButton.onclick = function() { editor.undo() };
+document.body.insertBefore(undoButton, editorplaceholder)
+
+var redoButton = document.createElement("button");
+redoButton.innerHTML = "Redo";
+redoButton.onclick = function() { editor.redo() };
+document.body.insertBefore(redoButton, editorplaceholder)
+
+var saveStatus = document.createElement("pre");
+saveStatus.id = "saveStatus"
+saveStatus.innerHTML = "not saved";
+saveStatus.style = "color: red"
+document.body.insertBefore(saveStatus, editorplaceholder);
+
+var refs = {
+  loadButton: loadButton,
+  undoButton: undoButton,
+  redoButton: redoButton,
+}
+
+
+
 editor.on("input", updateToolbar);
 editor.textInput.getElement().addEventListener("keyup", save);
+editor.textInput.getElement().addEventListener("keydown", setUnsave);
 
 editor.commands.addCommand({
     name: "save",
