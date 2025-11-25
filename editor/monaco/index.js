01a4ea296a9da3a06686e8e82e139ec7ff0e361a
Juan Camilo Florez <jcflorezv@unal.edu.co>
2025-11-25 15:00:21 -0500

    fix editor in global ctx for monaco

--- editor/monaco/index.js
+++ editor/monaco/index.js
@@ -18,7 +18,7 @@ self.MonacoEnvironment = {
 	}
 };
 
-var editor = monaco.editor.create(document.getElementById('editor'), {
+window.editor = monaco.editor.create(document.getElementById('editor'), {
 	value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
 	language: 'javascript',
   theme: "vs-dark",
--- system-setup.sh
+++ system-setup.sh
@@ -32,7 +32,10 @@ case $1 in
   npm install .
   npx webpack
   popd
-;;
+
+  ln -s editor/monaco/dist/ts.worker.bundle.js
+  ln -s editor/monaco/dist/editor.worker.bundle.js
+  ;;
 
 *)
   echo "Please select and option: 'monaco' or 'ace'"
@@ -49,26 +52,3 @@ chmod a+w tasks/food.tks     # apache server user has write access
 chmod 655 script/save.py     # apache server user can execute
 chmod 655 script/backup.py   # apache server user can execute
 chmod 655 script/load.py     # apache server user can execute
-
-
-
-
-exit
-
-
-exit
-ln -s monacoeditor/example/dist/index.html index.html
-ln -s monacoeditor/example/dist/index.js index.js
-ln -s monacoeditor/example/dist/index.css index.css
-
-#this is a dratf
-
-#giving execution permissions to apache
-chmod 655 script/save.py
-mkdir tasks
-touch tasks/main.tks
-chmod 666 tasks/main.tks
-
-#cd /etc/apache2/conf-enable
-#sudo ln -s ~/repo.m/taskseditor/serve-cgi-bin.conf serve-cgi-bin.conf
-
